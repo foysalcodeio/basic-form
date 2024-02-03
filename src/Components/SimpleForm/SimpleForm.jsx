3e36fe9649f35969d5d3eae17da7a01784beca5b
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-02-03 12:52:16 +0600

    basic form idea

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,16 +1,20 @@
 const SimpleForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault(); 
-        console.log('form submitted')
+        
         console.log(e.target.name.value);
-        console.log(e.target.email.value)
-
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value)
+        console.log('form submitted')
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
             <input type="text" name="name" />
+            <br />
             <input type="email" name="email" />
+            <br />
+            <input type="text" name="phone" id="" />
             <br />
             <input type="submit" value="submit" />
             </form>
